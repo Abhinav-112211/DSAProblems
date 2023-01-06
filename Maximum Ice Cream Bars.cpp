d2ac7e81ed00b29447e3834d4dc16e7243c5f5ac
Abhinav Bansal <abhinavbansal112211@gmail.com>
2023-01-06 16:21:19 +0530

    Add files via upload

---
+++ Maximum Ice Cream Bars.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(),costs.end());
+        int sum=0;
+        int count=0;
+        for(int i=0;i<costs.size();i++){
+            sum+=costs[i];
+            count++;
+            if(sum==coins)break;
+            if(sum>coins)return count-1;
+        }
+        return count;
+    }
+};
